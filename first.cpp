13b77a0e1c8b6b5153467f0bb8eea0e21a09b02a
HideOnBed1 <91094661+HideOnBed1@users.noreply.github.com>
2022-09-04 17:44:56 -0400

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+// void fun(int* n){
+//     if(*n>100){
+//         *n=100;
+//     }else if(*n>0){
+//         *n=0;
+//     }
+// }
+// 
+
+
+using namespace std;
+int main()
+{
+	cout << "version" << __cplusplus << endl;
+	return 0;
+}
